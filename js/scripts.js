8404ebcc84ede1d57abfcdb08a6eb7277d596a6f
Noah <nabelmont@me.com>
2017-10-30 20:46:47 -0700

    last push of class

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+function logMessage() {
+  console.log('Hello World!');
+};
+
+function doesSomething()  {
+  // psuedo code
+}
+// Logs a message when p tag is clicked
+
+
+/*
+this is a multi line
+comment
+*/
+
+var name = "Noah";
+
+// ES5 String Interpolation
+var message = "Hello there, " + name + "!";
+console.log(message);
+
+// ES6 String Interpolation
+var secondMessage = `Hello there, ${name}!`;
+console.log(secondMessage);
+
+
+// Variable Declaration
+
+
+// Variable Definition
+
+console.log(number);
+
+var number = 14;
