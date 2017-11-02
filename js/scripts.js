062d981c96ba199348b11185740eb32f12513425
Noah <nabelmont@me.com>
2017-11-01 20:50:04 -0700

    Pushing up End of Second Day Notes

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,89 @@
-function logMessage() {
-  console.log('Hello World!');
-};
+// function logMessage() {
+//   console.log('Hello World!');
+// };
+//
+// function doesSomething()  {
+//   // psuedo code
+// }
+// // Logs a message when p tag is clicked
+//
+//
+// /*
+// this is a multi line
+// comment
+// */
+//
+// var name = "Noah";
+//
+// // ES5 String Interpolation
+// var message = "Hello there, " + name + "!";
+// console.log(message);
+//
+// // ES6 String Interpolation
+// var secondMessage = `Hello there, ${name}!`;
+// console.log(secondMessage);
+//
+//
+// // Variable Declaration
+//
+//
+// // Variable Definition
+//
+// console.log(number);
+//
+// var number = 14;
+//
+//
+//
+// // {
+// //   This is a block of code!
+// // }
+// // function practice(){
+// // notDeclared = 'When it works how I expect it to...';
+// //
+// // var declared = "js is awesome!!";
+// //
+// // };
+// //
+// // practice();
+// //
+// // console.log(declared, notDeclared);
+// //
+// pizza = true;
+// //
+// // cheeseC = sauce;
+// // console.log(pizza, cheeseC)
+// console.log(pizza)
+// if(pizza) {
+//  let pizza = 78;
+//  console.log(pizza);
+// };
 
-function doesSomething()  {
-  // psuedo code
-}
-// Logs a message when p tag is clicked
+// const team = 55;
+// let team = 11;
+// var team = 'Dodgers';
+// console.log(team);
+//
+// var rightNow = new Date('3/19/1996');
+//
+//
+// var name = 'Noah';
+//
+// var stringObj = new String('Noah');
+//
+// console.log(name);
+// console.log(stringObj.split('o').join('ough'));
 
 
-/*
-this is a multi line
-comment
-*/
+let bestNumeber = 42;
 
-var name = "Noah";
+console.log(typeof bestNumber);
 
-// ES5 String Interpolation
-var message = "Hello there, " + name + "!";
-console.log(message);
+let reallyBestNumber = new Number(42);
 
-// ES6 String Interpolation
-var secondMessage = `Hello there, ${name}!`;
-console.log(secondMessage);
+console.log(reallyBestNumber);
 
 
-// Variable Declaration
+let bool = new Boolean(true);
 
-
-// Variable Definition
-
-console.log(number);
-
-var number = 14;
+console.log(bool);
