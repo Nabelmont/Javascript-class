5f9c5c2e1a1bb72460cbba5431cdfcf5c2112579
Noah <nabelmont@me.com>
2017-11-15 21:02:46 -0800

    Final push

--- js/scripts.js
+++ js/scripts.js
@@ -28,22 +28,22 @@
 //
 // doWork(task, time)
 
-var sentence = prompt('Give me a sentence!');
-
-function capitalize(string) {
-  if(string.indexOf('.') != -1) {
-    var example = string.split('.');
-    if(example.length > 1) {
-      let newSentence;
-      let i = 0;
-      for(i; i <= example.length; i++) {
-        var firstLetter = example[i].substr(0,1).toUpperCase();
-        var rest = example[i].substr(1).toLowerCase();
-          newSentence += `${firstLetter}${rest}`;
-      }
-      return newSentence;
-    }
-  }
+// var sentence = prompt('Give me a sentence!');
+//
+// function capitalize(string) {
+//   if(string.indexOf('.') != -1) {
+//     var example = string.split('.');
+//     if(example.length > 1) {
+//       let newSentence;
+//       let i = 0;
+//       for(i; i <= example.length; i++) {
+//         var firstLetter = example[i].substr(0,1).toUpperCase();
+//         var rest = example[i].substr(1).toLowerCase();
+//           newSentence += `${firstLetter}${rest}`;
+//       }
+//       return newSentence;
+//     }
+//   }
 
 
 
@@ -51,6 +51,41 @@ function capitalize(string) {
 //   var firstLetter = string.substr(0,1).toUpperCase();
 //   var rest = string.substr(1).toLowerCase();
 //   return `${firstLetter}${rest}`;
+// // };
+//
+// alert(capitalize(sentence));
+
+
+// var string = "aodsfhpa";
+//
+// var arr = string.split('abcdef');
+
+// var myId = document.querySelector('#hello');
+//
+// myId.addEventListener('click', ((e) => {
+//
+//   // do stuff here
+// });
+//
+// var myEventFn = function() {
+//
 // };
 
-alert(capitalize(sentence));
+
+// var isUser = confirm('are you a user?');
+// myId.addEventListener('click', isUser ? myEventFn : nonUserFn);
+
+var textWeights = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+var input = document.querySelector('#myInput');
+var body = document.querySelector('body');
+let myPTag = document.querySelector('#myPTag');
+input.addEventListener('keydown', (() => {
+  let userInput = e.target.value;
+  let r = Math.floor(Math.random() * (colors.length - 1) - 1 +1)) + 1;
+  let randColor = textWeights[r];
+  body.style.backgroundColor = randColor;
+  myPTag.innerHTML = userInput;
+  if(e.keycode == 13) {
+    
+  }
+}));
